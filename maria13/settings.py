136fc89d5e1fcdcce71e78e3a3a2f8f5c2339162
maria <maria.ugnich@gmail.com>
2021-03-12 01:10:52 +0700

    add css and js files

--- maria13/settings.py
+++ maria13/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('PORTFOLIO_SECRET_KEY')
+SECRET_KEY = os.environ['PORTFOLIO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
